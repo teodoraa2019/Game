d298d79550aee30dbf74c0a381bf7b299ab3d2bf
Teodora Vodopija <48692987+teodoraa2019@users.noreply.github.com>
2020-04-03 19:44:53 +0200

    Jos treba dogradnje
    urediti dizajn

---
+++ Carobnjak/Assets/Carobnjak.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Carobnjak : MonoBehaviour {
+
+    int max;
+    int min;
+    int Pokusaj;
+    int MaxBrojPokusaja = 10;
+
+    public Text text;
+    
+    void Start () {
+        StartGame();
+	}
+	
+    void StartGame() {
+        max = 1001;
+        min = 1;
+        NextPokusaj();
+    }
+	
+	public void pokusajLower () {
+        max = Pokusaj;
+        NextPokusaj();
+	}
+
+    public void pokusajHigher()
+    {
+        min = Pokusaj;
+        NextPokusaj();
+    }
+
+    void NextPokusaj () {
+        Pokusaj = Random.Range(min, max + 1);
+        text.text = Pokusaj.ToString();
+        MaxBrojPokusaja = MaxBrojPokusaja - 1;
+        if (MaxBrojPokusaja <= 0) {
+            Application.LoadLevel("Win");
+        }
+    }
+}
